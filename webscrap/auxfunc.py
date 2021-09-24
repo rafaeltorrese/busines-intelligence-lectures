48386ec03f24a8a591581f2a70d0686b4317ad46
Rafael Torres Escobar <torresc.rafael@gmail.com>
2021-09-23 21:11:22 -0500

    function to scrape all notes

--- webscrap/auxfunc.py
+++ webscrap/auxfunc.py
@@ -1,3 +1,5 @@
+import requests
+
 def get_notes(url, section):
     '''
     Function that returns a URL list to the notes in a section
@@ -14,40 +16,3 @@ def get_notes(url, section):
         return [featured_link] + [f"{url}{article.a.get('href')}" for article in article_list if article]
 
     return [f"{url}{article.a.get('href')}" for article in article_list if article]
-
-
-def get_info(soup_note):
-    info = {
-        'title': None,
-        'teaser': None,
-        'subheader': None,
-        'author': None,
-        'body': }
-
-    title = soup_note.find('div', attrs={'class': "col 2-col"}).find('h1')
-    if title:
-        info['title'] = title
-
-    # volanta
-    teaser = soup_note.find(
-        'div', attrs={'class': "col 2-col"}).find('h4')  # maybe None
-    if teaser:
-        info['teaser'] = teaser
-
-    # copete o bajada
-    subheader = soup_note.find(
-        'div', attrs={'class': "col 2-col"}).find('h3')  # maybe None
-    if subheader:
-        info['subheader'] = subheader
-
-    # author
-    author = soup_note.find('div', attrs={'class': "author-name"})
-    if author:
-        info['author'] = author
-
-    # cuerpo
-    body = soup_note.find(
-        'div', attrs={'class': "article-main-content article-text"}).find_all('p')
-    body_text = [b.get_text() for b in body]
-    if body:
-        info['body'] = body_text
